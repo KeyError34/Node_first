16f24c4ee8004b39cf50c65ffd9c3b2b7a1958d1
KeyError34 <150452663+KeyError34@users.noreply.github.com>
2024-10-29 08:29:50 +0100

    package

--- logger.js
+++ logger.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
-const log= './log.txt'
+const log = './log.txt';
 function logMessage(message) {
   const timestamp = new Date().toISOString();
   fs.appendFile(log, `${timestamp} - ${message}\n`, (err) => {
     if (err) {
       throw err;
     }
-  })
+  });
 }
 function clearLog() {
   fs.writeFile(log, '', (err) => {
     if (err) {
       throw err;
     }
-  })
+  });
 }
-module.exports={logMessage, clearLog}
+module.exports = { logMessage, clearLog };
